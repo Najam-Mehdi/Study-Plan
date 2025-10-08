3244e6d77453d8b13bde8cd038e088c633ccc9f3
SYED NAJAM MEHDI <166817245+Najam-Mehdi@users.noreply.github.com>
2025-10-08 02:16:46 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -1384,7 +1384,7 @@ def main():
 
                         # ---- show download button ----
                         st.download_button(
-                            "⬇ Download PDF",
+                            "⬇ Download PDF Copy",
                             data=pdf_bytes,
                             file_name=fname,
                             mime="application/pdf",
@@ -1399,7 +1399,7 @@ def main():
                             st.caption(f"Drive link: {resp['fileUrl']}")
 
                         # ---- show download only AFTER generation ----
-                        st.download_button("⬇ Download PDF Copy", data=pdf_bytes, file_name=fname, mime="application/pdf")
+                        #st.download_button("⬇ Download PDF Copy", data=pdf_bytes, file_name=fname, mime="application/pdf")
 
                 finally:
                     st.session_state.submitting_pdf = False
